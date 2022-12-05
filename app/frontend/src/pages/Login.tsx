0b1dedf5b77df53cf03834abeffcfc87fee8e5be
Rafhael Oliveira <rafhaelxd@gmail.com>
2022-12-05 14:14:08 -0300

    Dev: Configura React-Router-Dom@Types. Rota login e home no frontend
    Usando o react router dom @types para criar as rotas do front-end

---
+++ app/frontend/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import { NavBar } from "./components/NavBar";
+
+export const Login = () => {
+  return (
+    <Card>
+        Login Card
+        <NavBar/>
+    </Card>
+  );
+};

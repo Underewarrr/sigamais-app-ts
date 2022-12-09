19d6c50db0db978544bb46a05ee0e368d6566af7
Rafhael Oliveira <rafhaelxd@gmail.com>
2022-12-09 01:23:42 -0300

    Dev: Desenvolve regras de negocio do sistema de login
    Sistema de login usando o useState e useffect e fazendo a requisição no backend com o service request.

--- app/frontend/src/pages/Login.tsx
+++ app/frontend/src/pages/Login.tsx
@@ -1,10 +1,44 @@
-// import React, {useEffect, useState} from 'react'
-import * as Yup from 'yup';
+import React, { useEffect, useState } from 'react'
+import { requestLogin, setToken } from '../services/requests';
+
 // import axios from "axios";
 import { Alert, Container, Button, Card, Form } from 'react-bootstrap';
+import { Navigate } from 'react-router-dom';
 
 export const Login = () => {
-  
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLogged, setIsLogged] = useState(false);
+  const [failedTryLogin, setFailedTryLogin] = useState(false);  
+
+  const login = async (event: any) => {
+    event.preventDefault();
+
+    try {
+      const { token } = await requestLogin('/login', { email, password });
+
+      setToken(token);
+
+      localStorage.setItem('token',  token);
+      setIsLogged(true);
+    } catch (error) {
+      setTimeout(() => {
+      setFailedTryLogin(true);
+
+      }, 1000)
+      setIsLogged(false);
+    }
+  };
+
+  useEffect(() => {
+     setTimeout(() => {
+       setFailedTryLogin(false);
+     }, 1000)
+  }, [email, password])
+
+  if (isLogged) return <Navigate to="/panel" />;
+
   return (
   <Form>
   <Card>
@@ -16,7 +50,12 @@ export const Login = () => {
     <Card.Body>
       <Form.Group className="mb-3" controlId="FormasicEmail">
         <Form.Label>Email : </Form.Label>
-        <Form.Control name="email"type="email" placeholder=" Example@email.com" />
+        <Form.Control 
+        value={ email }
+        onChange={ ({ target: { value } }) => setEmail(value) }
+        name="email"
+        type="email" 
+        placeholder=" Example@email.com" />
         <br/>
         <Form.Text className="text-muted">
           Nunca compartilhe sua senha.
@@ -24,13 +63,34 @@ export const Login = () => {
       </Form.Group>
       <Form.Group className="mb-3" controlId="FormasicPassword">
         <Form.Label>Password : </Form.Label>
-        <Form.Control name="password" type="password" placeholder=" Password" />
+        <Form.Control 
+        value={ password }
+        onChange={ ({ target: { value } }) => setPassword(value) }
+        name="password" 
+        type="password" 
+        placeholder=" Password" />
       </Form.Group>
       </Card.Body>
       <Card.Footer>
-      <Button variant="primary" type="submit">
+      <Button 
+      variant="primary" 
+      type="submit"
+      onClick={ (event) => login(event) }
+      >
         Login
       </Button>
+      {
+            (failedTryLogin)
+              ? (
+                <Alert variant="danger">
+                  {
+                    `O endereço de e-mail ou a senha não estão corretos.
+                    Por favor, tente novamente.`
+                  }
+                </Alert>
+              )
+              : null
+          }
       </Card.Footer>
       </center>
       </Container>

8d9fe6589baa2108207d452bcefd3d6cd2b310da
Rafhael Oliveira <rafhaelxd@gmail.com>
2023-01-09 17:33:31 -0300

    Fix: Links errados navbar

--- app/frontend/src/pages/components/NavBar.tsx
+++ app/frontend/src/pages/components/NavBar.tsx
@@ -6,23 +6,21 @@ export const NavBar = () => {
   return (
             <Navbar bg="light" expand="lg">
             <Container>
-              <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+              <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                  <Nav.Link><Link to="/">Home</Link></Nav.Link>
+                  <Nav.Link href="/">Home</Nav.Link>
                   <Nav.Link href="#link">Link</Nav.Link>
                   <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">
-                      
-                    </NavDropdown.Item>
+                    <NavDropdown.Item href="/3.1">Action</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.2">
                       Another action
                     </NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                     <NavDropdown.Divider />
-                    <NavDropdown.Item>
-                      <Link to="/login">Login</Link>
+                    <NavDropdown.Item href="/login">
+                      Login
                     </NavDropdown.Item>
                   </NavDropdown>
                 </Nav>
